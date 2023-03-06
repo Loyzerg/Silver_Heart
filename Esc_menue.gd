9c8f7dd1ad158e390eef326028c29de2f916b631
GokaPek <109132407+GokaPek@users.noreply.github.com>
2023-03-06 23:28:31 +0400

    patch_0_0_2_4_1

---
+++ Esc_menue.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+
+func _ready():
+	pass 
+	
+
+var x = false
+func _input(event):
+	if event.is_action_pressed("ui_end") or event.is_action_pressed("ui_cancle") or ($Interface/Button.pressed):
+		if x == false:
+			$Interface.visible = true
+			get_tree().paused = true
+			x = true
+			return
+			
+		if x == true:
+			$Interface.visible = false
+			get_tree().paused = false
+			x = false
+			return
