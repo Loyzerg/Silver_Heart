bbf8410325dfbf6239958f39683489d9f0df64a6
DyCTaTOR <125912249+DyCTaTOR@users.noreply.github.com>
2023-03-08 14:32:27 +0400

    Revert "Revert "patch_0_0_2_5_1""
    This reverts commit 3b524bd7e4d773101b98c65485c4d570d5e72a6e.

--- Esc_menue.gd
+++ Esc_menue.gd
@@ -4,19 +4,21 @@ extends Node2D
 
 func _ready():
 	pass 
-	
+var Back_menue_class = load("res://Back_menue.gd")
 
-var x = false
+var TF = Back_menue_class.new()
+
+var esc_menue_bool = false
 func _input(event):
-	if event.is_action_pressed("ui_end") or event.is_action_pressed("ui_cancel") or ($Interface/Button.pressed):
-		if (x == false) and (get_tree().paused == false):
+	if (event.is_action_pressed("ui_end") or event.is_action_pressed("ui_cancel") or ($Interface/Button.pressed)) and (TF.back_menue_bool == true):
+		if (esc_menue_bool == false) and (get_tree().paused == false):
 			$Interface.visible = true
 			get_tree().paused = true
-			x = true
+			esc_menue_bool = true
 			return
 			
-		if x == true:
+		if esc_menue_bool == true:
 			$Interface.visible = false
 			get_tree().paused = false
-			x = false
+			esc_menue_bool = false
 			return
