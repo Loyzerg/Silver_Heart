bb5535e4cf418c598537ecf31adb4f5972b94060
GokaPek <109132407+GokaPek@users.noreply.github.com>
2023-03-28 23:34:31 +0400

    patch_0_0_2_7_6
    Кнопка в статьях глоссария для отображения изображений

--- nodes/Gloss_block.gd
+++ nodes/Gloss_block.gd
@@ -3,3 +3,8 @@ extends TextureRect
 func set_gloss(name, inf):
 	$RichTextLabel.text = inf
 	$Label.text = name
+func _input(event):
+	name = $Label.text
+	if ($Button.pressed == true):
+		var im = load("res://sprites/glossimages/%s.png" % name)
+		get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_node("Image").set_texture(im)
